1bce617969c1fea21cd08ec86a1e2dff7efa1498
Panbouh <cy.catoire@gmail.com>
2020-10-06 21:19:47 +0200

    rework of the bttle process to make it more interractive

---
+++ Watata_run/trash.swift
@@ -0,0 +1,47 @@
+//
+//  trash.swift
+//  Watata_run
+//
+//  Created by bouh on 06/10/2020.
+//  Copyright © 2020 bouh. All rights reserved.
+//
+
+import Foundation
+
+////ask player with what moto attak
+//print("Select your runner\n")
+//P1.printMotoInLife()
+//var runner = Tools.shared.makeChoiceInt(between: 1...P1.motoInLife.count) //On a le droit de faire ca?? :o (range)
+//
+////ask player who attack with
+//print("Select your oppenent\n")
+//P2.printMotoInLife()
+//var oppenent = Tools.shared.makeChoiceInt(between: 1...P1.motoInLife.count)
+//
+////lunch run
+//run(runner: runner, oppenent: oppenent)
+//whoTurn = false
+
+
+//print("------- \(attak.name) turn. team : \(attak.motoInLife.count)----\n")
+//
+//let boostChest = Bool.random()
+//
+//
+//guard let bam = attak.motoInLife.first else {return}
+//guard let aie = victim.motoInLife.first else {return}
+//
+//let oldMotor = bam.motor
+//if boostChest {
+//	print("+++++++++++++++++++++\nOh! A treasure chest is appear, it contain a COMPRESSOR\n"
+//		+ "\(attak.name) Equiped it on his bike!\n++++++++++++++++++\n")
+//	bam.motor = Compressor()
+//}
+//
+//bam.gaz(target: aie, player: victim)
+//
+//if (boostChest) {
+//	print("\(attak.name) lost his compressor")
+//	bam.motor = oldMotor
+//}
+//print("------------------------\n")
